7e5ca4c7c94b4d4151ef4d442192f7eb91f9b3be
Adam Szymajda <adam.szymajda@gmail.com>
2020-09-16 19:49:23 +0200

    Added for loop

--- mod3task1.py
+++ mod3task1.py
@@ -1,4 +1,7 @@
 shopping_list = {
-    Monopolowy : [piwo, wino, whisky]
-    Żabka : [ogórki, śledzie, chipsy]
+    "Monopolowy": ["piwo", "wino", "whisky"],
+    "Żabka": ["ogórki", "śledzie", "cola"]
 }
+
+for shop, item in shopping_list.items():
+    print("Idę do {} i kupuję tam {}".format(shop, item))
