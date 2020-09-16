1c44e0ee2a355356ef6ec897ceb507394467da65
Adam Szymajda <adam.szymajda@gmail.com>
2020-09-16 20:19:48 +0200

    Final(??) version

--- mod3task1.py
+++ mod3task1.py
@@ -3,6 +3,13 @@ shopping_list = {
     "Żabka": ["ogórki", "śledzie", "cola"]
 }
 
+counter = 0
+
 for shop, item in shopping_list.items():
     items = ', '.join(article for article in item)
     print("Idę do {} i kupuję tam {}".format(shop.upper(), items.upper()))
+
+for key in shopping_list:
+    counter += len(shopping_list[key])
+
+print("W sumie kupuję {} produktów".format(counter))
